b78c8f0017007d1cbf01433c173aaf2b117b6b08
dimitris <dimitris@pop-os.localdomain>
2021-02-14 07:54:38 +0200

    first commit

---
+++ exercises/error_handling/result1.rs
@@ -0,0 +1,41 @@
+// result1.rs
+// Make this test pass! Execute `rustlings hint result1` for hints :)
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        //first solution
+        //if value > 0{
+        //    Ok(PositiveNonzeroInteger(value as u64))
+        //}else if value == 0{
+        //    Err(CreationError::Zero)
+        //}else{
+        //    Err(CreationError::Negative)
+        //} 
+        
+        //second solution
+        match value{
+            val if val > 0 => Ok(PositiveNonzeroInteger(value as u64)),
+            val if val == 0 => Err(CreationError::Zero),
+            _ => Err(CreationError::Negative)
+        } 
+    }
+}
+
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
